738b325b896fdc46c15cf3accbafe5b62c2667dc
Sangwoo Park <Johnpark@Johnssonslab.info>
2022-09-01 11:20:38 +0900

    [UPDATE] Image control api - baseline

--- db_image/app.py
+++ db_image/app.py
@@ -1,6 +1,33 @@
+import json
 
 
 
+
+
+
+
+def respond(err, res=None, step=None): ### error 
+    ### boto3 code convention와 양식을 일치시키기 위해서
+    TotalCnt = 0
+    if type(res) is list:
+        TotalCnt = len(res)    
+    #print(f"BODY : {json.dumps(res,ensure_ascii=False)}")
+    meta={
+             'status': err.message if err else 'normal_condition',
+             'step': step,
+             'TotalCnt': TotalCnt
+        }
+    
+    return {
+        'statusCode': '400' if err else '200',
+        'body': json.dumps( {
+            "items":'[]',
+            "meta" :meta
+            },ensure_ascii=False) if err else json.dumps(res,ensure_ascii=False),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
 
 
 def lambda_handler(event,context):
